c058ab5addcda9d514bd054d056f6768cf31b0c5
SeriouS <89145685+onium16@users.noreply.github.com>
2023-06-15 19:00:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, Response
+from bot import start_repost_bot
+import threading
 
 app = Flask(__name__)
 
+BOT_TOKEN = os.environ.get('TOKEN') 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/")
+def hello():
+    # return Response('<h1>Hello, this is Telegram Bot!</h1>', mimetype='text/html')
+    return f'{BOT_TOKEN}'
 
+@app.route("/start_bot", methods=["GET"])
+def start_bot():
+    # Running a bot in a separate thread
+    bot_thread = threading.Thread(target=start_repost_bot)
+    print('Start checking')
+    bot_thread.start()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    return Response('<h1>Bot started successfully!</h1>', mimetype='text/html')
+
+if __name__ == "__main__":
+    app.run()
